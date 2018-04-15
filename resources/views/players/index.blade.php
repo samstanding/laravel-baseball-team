94defc5b7d8c569a1fa546bc3ecbaf668930fae2
Sam Standing <samzstanding@gmail.com>
2018-04-15 13:21:51 -0500

    added auth, changed views to direct to players index

---
+++ resources/views/players/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+        <h1>Welcome to Your Team</h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+@endsection
